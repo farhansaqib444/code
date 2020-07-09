d32c8cf776aa6d040157cd2260b708ec8eb5677a
bendevjb <bendev@jobesk.com>
2020-07-09 12:13:50 +0500

    responsive

--- js/themeJs.js
+++ js/themeJs.js
@@ -8,3 +8,27 @@ $(function() {
     })
 });
 
+
+$(document).ready(function(){
+    $(".owl-carousel.trips-owl").owlCarousel({
+        loop:true,
+        margin:25,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+                nav: true,
+            },
+            1024:{
+                items:3,
+                nav: true,
+            },
+            1200:{
+                items:4,
+                nav: true,
+            }
+        }
+    });
+});
